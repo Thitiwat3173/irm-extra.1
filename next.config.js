6b37395e8f0227aac85bf997dc7557cbcae4b692
Thitiwat3173 <138146352+Thitiwat3173@users.noreply.github.com>
2026-08-21 14:50:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  setupDevPlatform().catch(console.error);
-}
 
 module.exports = nextConfig;
